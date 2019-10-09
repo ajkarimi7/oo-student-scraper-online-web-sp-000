edb63bfd82f89aa2f6e5e3d86e4bb90e8977ea90
Ali Karimi <ajkarimi7@gmail.com>
2019-10-09 10:34:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,8 +3,9 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
+  def self.scrape_index_page(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html)
   #respnsible for scraping the index page that lists all of the students
+  
     
   end
 
