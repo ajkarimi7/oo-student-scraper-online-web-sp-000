40332623fa02cc36c8dd672b0bf38f07a6e49ac8
Ali Karimi <ajkarimi7@gmail.com>
2019-10-09 10:34:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+  #respnsible for scraping the index page that lists all of the students
     
   end
 
   def self.scrape_profile_page(profile_url)
-    
+  #respnsible for scraping an individual students profile page to get further information about the student
   end
 
 end
